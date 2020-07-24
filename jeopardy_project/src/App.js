537e924b663218bd78e7773d72f6ed03bb6258d1
Alvin Tang <tangalvin4@gmail.com>
2020-07-24 16:19:53 -0400

    removed background image added component

--- jeopardy_project/src/App.js
+++ jeopardy_project/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Box from './components/Box';
 
 function App() {
   return (
     <div className="container">
-      
+      <h1>JEOPARDY</h1>
+      <Box></Box>
     </div>
   );
 }
---
+++ jeopardy_project/src/components/Box.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Box extends Component{
+    render(){
+        return(
+            <div style={{outlineStyle:"solid",outlineColor:"black"}}>
+                <h1>TOPIC</h1>
+            </div>
+        )
+    }
+}
+export default Box;
